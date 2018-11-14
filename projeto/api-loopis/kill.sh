73ce2bd3870b1e91bba282857a251eda22903297
FlavioHenriique <flaviohenrique638@gmail.com>
2018-11-13 21:23:07 -0300

    Organizando pastas

--- projeto/api-loopis/kill.sh
+++
@@ -1,4 +0,0 @@
-
-sudo docker kill $(sudo docker ps -aq --filter name=loopisdb)
-sudo docker rm -f $(sudo docker ps -aq --filter name=loopisdb)
-
--- projeto/api-loopis/run.sh
+++
@@ -1,3 +0,0 @@
-sudo docker-compose up -d
-# docker build -t loopis/db database/
-#docker run -p 5431:5432 --name loopisdb loopis/db
