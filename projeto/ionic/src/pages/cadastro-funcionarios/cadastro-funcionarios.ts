c17be115062991775913a2c3f94d00b7852c54ce
FlavioHenriique <flaviohenrique638@gmail.com>
2018-12-10 13:07:30 -0300

    Adicionando verificação e mostrando alert quando o email do funcionário já for  cadastrado

--- projeto/ionic/src/pages/cadastro-funcionarios/cadastro-funcionarios.ts
+++ projeto/ionic/src/pages/cadastro-funcionarios/cadastro-funcionarios.ts
@@ -3,7 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Funcionario } from '../../model/Funcionario';
 import { HttpClient } from '@angular/common/http';
 import { AlertController } from 'ionic-angular';
-import {Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 /**
  * Generated class for the CadastroFuncionariosPage page.
@@ -21,16 +21,15 @@ export class CadastroFuncionariosPage {
 
   root: boolean;
   funcionario: Funcionario;
-  validations_form : FormGroup;
+  validations_form: FormGroup;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public http: HttpClient,
     private alertCtrl: AlertController,
-    private formBuilder: FormBuilder) 
-    {
+    private formBuilder: FormBuilder) {
     this.funcionario = new Funcionario();
-    
+
   }
 
   ngOnInit(): any {
@@ -43,17 +42,17 @@ export class CadastroFuncionariosPage {
         Validators.required,
         Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
       ]))
-   });
+    });
   }
 
-  cadastrar(){
+  cadastrar() {
     this.funcionario.tipo = (this.root) ? 'ROOT' : 'NORMAL';
-    let url = "http://localhost:8081/funcionarios/cadastrar";
+    let url = "http://localhost:8081/funcionarios/";
     this.http.post(url, this.funcionario, { observe: 'response' }).subscribe(res => {
-      if (res.status != 200) {
-        this.presentAlert();
+      if (res.status == 202) {
+        this.presentAlert("Erro","Email já cadastrado!");
       } else {
-        console.log("foi");
+        this.presentAlert("OK!","Funcionário cadastrado com sucesso!");
       }
     });
   }
@@ -62,10 +61,10 @@ export class CadastroFuncionariosPage {
     this.navCtrl.pop();
   }
 
-  presentAlert() {
+  presentAlert(titulo, msg) {
     let alert = this.alertCtrl.create({
-      title: 'Error',
-      subTitle: 'Algo de errado não está certo',
+      title: titulo,
+      subTitle: msg,
       buttons: ['Ok']
     });
     alert.present();
--- projeto/ionic/www/build/main.js
+++ projeto/ionic/www/build/main.js
@@ -26,15 +26,15 @@ var map = {
 		3
 	],
 	"../pages/funcionarios/funcionarios.module": [
-		278,
+		276,
 		2
 	],
 	"../pages/inicial/inicial.module": [
-		276,
+		277,
 		1
 	],
 	"../pages/modal/modal.module": [
-		277,
+		278,
 		0
 	]
 };
@@ -116,7 +116,7 @@ var LoginPage = /** @class */ (function () {
     };
     LoginPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-login',template:/*ion-inline-start:"/home/loopis/Documents/Mailson/Ionic/projeto-loopis/projeto/ionic/src/pages/login/login.html"*/'<ion-header>\n  <ion-navbar color="primary">\n    <ion-title>LOOPIS</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding class="action-sheets-basic-page">\n    <ion-grid>\n       <ion-row justify-content-center align-items-center>\n         <h1 id="title">Nome do App</h1>\n       </ion-row>\n    </ion-grid>\n    \n    <div class="form-box">\n      <ion-row  align-items-center justify-content-center>\n          \n        <ion-list class="form">\n\n            <!-- <ion-img width="30" height="30" src="../assets/icon/user.png" ></ion-img> -->\n            <ion-item>\n              \n              <ion-label floating> <ion-icon name="person"></ion-icon>  Email</ion-label>\n              <ion-input type="text" [(ngModel)]="funcionario.email"></ion-input>\n            </ion-item>\n            \n            <ion-item>\n              <ion-label floating><ion-icon name="lock"></ion-icon>  Senha</ion-label>\n              <ion-input type="password" [(ngModel)]="funcionario.senha"></ion-input>\n            </ion-item>\n            \n            <br/><br/>\n            <ion-item>\n                <ion-label>ROOT</ion-label>\n                <ion-checkbox color="blue" checked="true" [(ngModel)]="root"></ion-checkbox>\n              </ion-item>\n          </ion-list>  \n      </ion-row>\n      \n      <br/><br/><br/>\n      <ion-row justify-content-center align-items-center>\n          <button class="btn" (click)="login()" ion-button>ENTRAR</button>\n          <button class="btn" ion-button (click)="cadastrarFuncionario()">Cadastro</button>\n          <button class="btn" ion-button (click)="proximas()">Funcionarios</button>\n      </ion-row>\n    </div>  \n</ion-content>\n'/*ion-inline-end:"/home/loopis/Documents/Mailson/Ionic/projeto-loopis/projeto/ionic/src/pages/login/login.html"*/
+            selector: 'page-login',template:/*ion-inline-start:"/home/flavio/ADS/Loopis/projeto-loopis/projeto/ionic/src/pages/login/login.html"*/'<ion-header>\n  <ion-navbar color="primary">\n    <ion-title>LOOPIS</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding class="action-sheets-basic-page">\n    <ion-grid>\n       <ion-row justify-content-center align-items-center>\n         <h1 id="title">Nome do App</h1>\n       </ion-row>\n    </ion-grid>\n    \n    <div class="form-box">\n      <ion-row  align-items-center justify-content-center>\n          \n        <ion-list class="form">\n\n            <!-- <ion-img width="30" height="30" src="../assets/icon/user.png" ></ion-img> -->\n            <ion-item>\n              \n              <ion-label floating> <ion-icon name="person"></ion-icon>  Email</ion-label>\n              <ion-input type="text" [(ngModel)]="funcionario.email"></ion-input>\n            </ion-item>\n            \n            <ion-item>\n              <ion-label floating><ion-icon name="lock"></ion-icon>  Senha</ion-label>\n              <ion-input type="password" [(ngModel)]="funcionario.senha"></ion-input>\n            </ion-item>\n            \n            <br/><br/>\n            <ion-item>\n                <ion-label>ROOT</ion-label>\n                <ion-checkbox color="blue" checked="true" [(ngModel)]="root"></ion-checkbox>\n              </ion-item>\n          </ion-list>  \n      </ion-row>\n      \n      <br/><br/><br/>\n      <ion-row justify-content-center align-items-center>\n          <button class="btn" (click)="login()" ion-button>ENTRAR</button>\n          <button class="btn" ion-button (click)="cadastrarFuncionario()">Cadastro</button>\n          <button class="btn" ion-button (click)="proximas()">Funcionarios</button>\n      </ion-row>\n    </div>  \n</ion-content>\n'/*ion-inline-end:"/home/flavio/ADS/Loopis/projeto-loopis/projeto/ionic/src/pages/login/login.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavController */],
             __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* Platform */],
@@ -209,9 +209,9 @@ var AppModule = /** @class */ (function () {
                 __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["e" /* IonicModule */].forRoot(__WEBPACK_IMPORTED_MODULE_6__app_component__["a" /* MyApp */], {}, {
                     links: [
                         { loadChildren: '../pages/cadastro-funcionarios/cadastro-funcionarios.module#CadastroFuncionariosPageModule', name: 'CadastroFuncionariosPage', segment: 'cadastro-funcionarios', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/funcionarios/funcionarios.module#FuncionariosPageModule', name: 'FuncionariosPage', segment: 'funcionarios', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/inicial/inicial.module#InicialPageModule', name: 'InicialPage', segment: 'inicial', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/modal/modal.module#ModalPageModule', name: 'ModalPage', segment: 'modal', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/funcionarios/funcionarios.module#FuncionariosPageModule', name: 'FuncionariosPage', segment: 'funcionarios', priority: 'low', defaultHistory: [] }
+                        { loadChildren: '../pages/modal/modal.module#ModalPageModule', name: 'ModalPage', segment: 'modal', priority: 'low', defaultHistory: [] }
                     ]
                 })
             ],
@@ -269,7 +269,7 @@ var MyApp = /** @class */ (function () {
         });
     }
     MyApp = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"/home/loopis/Documents/Mailson/Ionic/projeto-loopis/projeto/ionic/src/app/app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"/home/loopis/Documents/Mailson/Ionic/projeto-loopis/projeto/ionic/src/app/app.html"*/
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"/home/flavio/ADS/Loopis/projeto-loopis/projeto/ionic/src/app/app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"/home/flavio/ADS/Loopis/projeto-loopis/projeto/ionic/src/app/app.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["j" /* Platform */], __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */], __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */]])
     ], MyApp);
